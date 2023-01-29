da912adeaed3f85b7d9f9778e6fe8fb4e8df38a4
kimnewbie <gracecancho@gmail.com>
2023-01-29 23:16:46 +0900

    dark mode 적용 중

--- pages/components/home/animation.js
+++ pages/components/home/animation.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 import Lottie from 'react-lottie-player'
-// Alternatively:
-// import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
 
 import lottieJson from '/public/animation.json'
 
@@ -12,6 +10,7 @@ export default function Animation() {
       loop
       animationData={lottieJson}
       play
+      style={{ width: `100%`, height: `100%` }}
     />
   )
 }
--- pages/components/home/hero.js
+++ pages/components/home/hero.js
@@ -8,10 +8,13 @@ export default function Hero() {
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Hi There,
           <br className="hidden lg:inline-block" />I'm Youjin Kim
         </h1>
-        <p className="mb-8 leading-relaxed">인생을 작고 위하여서 싹이 꽃 열매를 이것이다. 튼튼하며, 얼마나 실현에 방지하는 그들은 인생에 얼마나 별과 보이는 아름다우냐? 길지 낙원을 인도하겠다는 능히 피고, 인간의 무엇을 봄바람이다. 못할 때에, 인간의 그러므로 현저하게 같지 뿐이다. 있으며, 그들을 청춘에서만 것은 봄날의 있는가? 같이 못하다 군영과 실로 목숨이 눈에 꾸며 되려니와, 있다. 따뜻한 커다란 역사를 이것이야말로 피다. 영원히 현저하게 가치를 그들에게 약동하다. 가는 열락의 피고, 같은 이상이 것은 위하여서. 바이며, 따뜻한 우리 끓는다. 따뜻한 위하여, 피는 싹이 못하다 이것이야말로 부패뿐이다.</p>
+        <p className="mb-8 leading-relaxed">인생을 작고 위하여서 싹이 꽃 열매를 이것이다. 튼튼하며, 얼마나 실현에 방지하는 그들은 인생에 얼마나 별과 보이는 아름다우냐? 길지 낙원을 인도하겠다는 능히 피고, 인간의 무엇을 봄바람이다. 못할 때에, 인간의 그러므로 현저하게 같지 뿐이다. 있으며, 그들을 청춘에서만 것은 봄날의 있는가? 같이 못하다 군영과 실로 목숨이 눈에 꾸며 되려니와, 있다. 따뜻한 커다란 역사를 이것이야말로 피다. 영원히 현저하게 가치를 그들에게 약동하다. 가는 열락의 피고, 같은 이상이 것은 위하여서. 바이며, 따뜻한 우리 끓는다. 따뜻한 위하여, 피는 싹이 못하다 이것이야말로 부패뿐이다.
+        </p>
         <div className="flex justify-center">
           <Link href="/projects" legacyBehavior>
-            <a className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">포트폴리오 보러가기</a>
+            <a className="btn-project">
+              포트폴리오 보러가기
+            </a>
           </Link>
         </div>
       </div>
