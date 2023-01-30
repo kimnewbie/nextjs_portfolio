4d63e9fceb9f312138cbb0d5f8940d3bf004b81f
kimnewbie <gracecancho@gmail.com>
2023-01-31 01:25:34 +0900

    connecting with notion api

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import Layout from "./components/layout";
+import { TOKEN, DATABASE_ID } from '../config';
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  /* 클라이언트(브라우저)에서 보임 */
+  // console.log(projects)
+
   return (
     <Layout>
       <Head>
@@ -9,7 +13,54 @@ export default function Projects() {
         <meta name="description" content="킴뉴진의 NEXT 세계" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Project</h1>
+      <h1>Whole Project : {projects.results.length}</h1>
+      {
+        projects.results.map((aProject, index) => (
+          <h1 key={index}>
+            {aProject.properties.Name.title[0].plain_text}
+          </h1>
+        ))
+      }
     </Layout>
   )
+}
+
+/* bulid time에 호출 (한 번만 실행) */
+export async function getStaticProps(context) {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`
+    },
+    body: JSON.stringify({
+      // filter: 'string',
+      // start_cursor: 'string',
+      sorts: [
+        {
+          "property": "Name",
+          "direction": "descending"
+        }
+      ],
+      page_size: 100
+    })
+  };
+
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
+
+  const projects = await res.json();
+
+  /* DB 표에 빈공간 있으면 plain_text가 undefined라서 error */
+  const projectNames = projects.results.map((aProject) => {
+    return aProject.properties.Name.title[0].plain_text
+  });
+
+  /* 서버에서 보임 */
+  // console.log(projectNames)
+
+  return {
+    props: { projects }, // will be passed to the page component as props
+  }
 }
